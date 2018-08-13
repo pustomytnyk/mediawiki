21e7fc256a96337640945e587bd8841b9c4a17c7
Roman Sokhan <pustomytnyk@gmail.com>
2018-08-14 00:29:29 +0300

    Add ParserFunctions

--- LocalSettings.php
+++ LocalSettings.php
@@ -144,3 +144,4 @@ require_once "$IP/extensions/Wikibase/repo/Wikibase.php";
 require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
 require_once "$IP/extensions/Wikibase/client/WikibaseClient.php";
 require_once "$IP/extensions/Wikibase/client/ExampleSettings.php";
+wfLoadExtension( 'ParserFunctions' );
