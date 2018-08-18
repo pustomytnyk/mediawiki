35def2c4eef16244d65a11f8ae41516d90098327
Roman Sokhan <pustomytnyk@gmail.com>
2018-08-18 14:03:43 +0300

    update

--- LocalSettings.php
+++ LocalSettings.php
@@ -22,7 +22,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
 
-$wgSitename = "Динопедія";
+$wgSitename =  [
+    'default' => 'Dinopedia',
+    'datawiki' => 'Dinopedia Data',
+  ];
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
@@ -32,7 +35,10 @@ $wgSitename = "Динопедія";
 $wgScriptPath = "";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "https://dinopedia-uk.herokuapp.com";
+$wgServer = [
+    'dinopedia' => 'https://dinopedia-uk.herokuapp.com',
+    'datawiki' => 'https://dinopedia-uk.herokuapp.com',
+  ];
 
 ## The URL path to static resources (images, scripts, etc.)
 $wgResourceBasePath = $wgScriptPath;
